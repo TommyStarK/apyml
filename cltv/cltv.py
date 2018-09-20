81b1c47c5e35e8adedef8f4a147472bbe338608f
Tommy <thomasmilox@gmail.com>
2018-09-20 09:17:40 +0200

    added demo

--- cltv/cltv.py
+++ cltv/cltv.py
@@ -50,16 +50,29 @@ class CLTV(object):
     def _predict_routine(self, path: str, models: list):
         from .internal import merkle_root
         h = merkle_root(self._dataframe.columns)
+        print(h)
         if h not in models:
             raise RuntimeError('No model found for this kind of dataframe')
         target = models[models.index(h)]
         import pickle
         model = pickle.load(open(f'{path}/{target}', 'rb'))
         preds = model.predict(self._dataframe)
-        print(self._dataframe.shape)
-        print(len(preds))
-        print(preds)
-        print(type(preds))
+        # Manouchage en vu de la demo
+        import pandas
+        spreds = pandas.Series(preds)
+        res = pandas.read_csv('~/Downloads/itm_sales_measured.csv')
+        print(res.head())
+        from sklearn.metrics import mean_squared_error
+        print(f"MSE -> {mean_squared_error(res['future_sales'], preds)}")
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+        ax.scatter(res['future_sales'], spreds, edgecolors=(0, 0, 0))
+        ax.plot([res['future_sales'].min(), res['future_sales'].max()], [res['future_sales'].min(), res['future_sales'].max()], 'k--', lw=4)
+        ax.set_xlabel('Measured')
+        ax.set_ylabel('Predicted')
+        plt.show()
+
+
 
     def _predict(self):
         self.config = context.get_config('predict')
